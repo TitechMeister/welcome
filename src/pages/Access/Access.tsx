7cc4105baa53b2e13c8e52e6a7dd9f1bbadf02f5
Zeu <tan2005kou@gmail.com>
2025-04-05 23:18:52 +0900

    文章の文字化けを直した

--- src/pages/Access/Access.tsx
+++ src/pages/Access/Access.tsx
@@ -7,15 +7,15 @@ function Access() {
             <Typography variant='h4'>アクセス</Typography>
             <img src={img_map} width={window.innerWidth * 0.8}></img>
             <Typography>
-                大岡山�?から東工大の正門に入り、左の道路に入って5�?ほど歩くと、セブンイレブンの真向かいに青い扉があります�?
-                こ�?�青い扉がMeisterの活動�?�所(倉庫)です�?Meisterに興味のある方、質問�?�ある方、是非見学に来て下さ�??�?
+                大岡山から東工大の正門に入り、左の道路に入って5分ほど歩くと、セブンイレブンの真向かいに青い扉があります。
+                この青い扉がMeisterの活動場所(倉庫)です。Meisterに興味のある方、質問がある方、是非見学に来て下さい。
             </Typography>
             <Typography>
-            直接活動�?�所に来ても大丈夫ですが�?部員がいな�?こともあるため、できる�?け事前に連絡�?(
-                  <a href="mailto:meisterwelcome2021@gmail.com">メール</a>�?
-                  <a href="https://twitter.com/Meister_2023">Twitter</a>�?
+            直接活動場所に来ても大丈夫ですが、部員がいないこともあるため、できるだけ事前に連絡先(
+                  <a href="mailto:meisterwelcome2021@gmail.com">メール</a>、
+                  <a href="https://twitter.com/Meister_2023">Twitter</a>、
                   <a href="https://liff.line.me/1645278921-kWRPP32q/?accountId=722fzwlk">LINE</a>
-                  の�?ずれ�? )まで連絡をしてください�?
+                  のいずれか )まで連絡をしてください。
             </Typography>
         </div>
     )
