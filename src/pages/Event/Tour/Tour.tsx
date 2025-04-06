643d0d69c8d59a3855924adf394cf858b373c102
Zeu <tan2005kou@gmail.com>
2025-04-06 21:32:14 +0900

    電操の作業体験の日程について

--- src/pages/Event/Tour/Tour.tsx
+++ src/pages/Event/Tour/Tour.tsx
@@ -20,7 +20,7 @@ export default function Tour() {
                         <ListItem>Pフレーム班 : 4/8、4/9、4/13、4/16</ListItem>
                         <ListItem>プロペラ班 : 4/9、4/10、4/16、4/17</ListItem>
                         <ListItem>駆動班 : 4/10、4/17</ListItem>
-                        <ListItem>電操班 : 4/7、4/10、4/11、4/14、4/17、4/18</ListItem>
+                        <ListItem>電操班 : 平日午後(具体的日時は相談のうえ決めます)</ListItem>
                         <ListItem>フェアリング班 : 4/7、4/14</ListItem>
                     </List>
                     <Button variant="outlined" color="primary" href="https://docs.google.com/forms/d/e/1FAIpQLSd93cDtUd5lHvC5iaYGXaGag2spyG6BOaS7FxnMroljVNN2jA/viewform" target="_blank" sx={{ padding: 2, alignSelf: "center" }}>参加申し込み</Button>
